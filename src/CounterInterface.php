840c9e15b03632f825735a659dcc53f3ecfbe8e4
Jordi Boggiano <j.boggiano@seld.be>
2016-11-13 18:44:02 +0100

    Update interfaces according to latest spec version

--- src/CounterInterface.php
+++ src/CounterInterface.php
@@ -1,23 +1,28 @@
 <?php
+
 namespace Psr\SimpleCache;
 
 interface CounterInterface
 {
     /**
-     * Increment a value atomically in the cache by its step value, which defaults to 1
+     * Increment a value atomically in the cache by the given step value and return the new value
      *
-     * @param string  $key  The cache item key
-     * @param int $step The value to increment by, defaulting to 1
+     * If the key does not exist, it is initialized to the value of $step
+     *
+     * @param string $key  The cache item key
+     * @param int    $step The value to increment by, defaulting to 1
      *
      * @return int|bool The new value on success and false on failure
      */
     public function increment($key, $step = 1);
 
     /**
-     * Decrement a value atomically in the cache by its step value, which defaults to 1
+     * Decrement a value atomically in the cache by the given step value and return the new value
      *
-     * @param string  $key  The cache item key
-     * @param int $step The value to decrement by, defaulting to 1
+     * If the key does not exist, it is initialized to the value of -$step
+     *
+     * @param string $key  The cache item key
+     * @param int    $step The value to decrement by, defaulting to 1
      *
      * @return int|bool The new value on success and false on failure
      */
