1289b71e4bc151c28c423f4f0784fce260377bfa
Ronan Giron <ElGigi@users.noreply.github.com>
2018-11-19 15:58:41 +0100

    PHP 5 backward compatibility

--- src/CacheException.php
+++ src/CacheException.php
@@ -3,6 +3,13 @@
 namespace Psr\SimpleCache;
 
 /**
+ * PHP 5 compatibility for \Throwable interface.
+ */
+if (version_compare(PHP_VERSION, '7.0.0', 'lt')) {
+    class_alias('Psr\SimpleCache\ThrowablePhp5', '\Throwable');
+}
+
+/**
  * Interface used for all types of exceptions thrown by the implementing library.
  */
 interface CacheException extends \Throwable
