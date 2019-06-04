0780ef664fcdc2a0cb565d08142e9c3822a3bba5
Niklas Ekman <niklas@stockfiller.com>
2019-06-04 17:06:11 +0200

    Implement interface

--- src/NullCache.php
+++ src/NullCache.php
@@ -10,40 +10,54 @@ class NullCache implements CacheInterface
     public function get($key, $default = null)
     {
         // noop
+        return $default;
     }
 
     public function set($key, $value, $ttl = null)
     {
         // noop
+        return true;
     }
 
     public function delete($key)
     {
         // noop
+        return true;
     }
 
     public function clear()
     {
         // noop
+        return true;
     }
 
     public function getMultiple($keys, $default = null)
     {
         // noop
+        $values = [];
+
+        foreach ($keys as $key) {
+            $values[$key] = $default;
+        }
+
+        return $values;
     }
 
     public function setMultiple($values, $ttl = null)
     {
         // noop
+        return true;
     }
 
     public function deleteMultiple($keys)
     {
         // noop
+        return true;
     }
 
     public function has($key)
     {
         // noop
+        return false;
     }
 }
