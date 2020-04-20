4913802d3783ee91b7881e0cafd32e24d3756308
Ronan Giron <ElGigi@users.noreply.github.com>
2020-04-20 15:52:01 +0200

    Remove PHP5 compatibility

--- src/CacheException.php
+++ src/CacheException.php
@@ -3,13 +3,6 @@
 namespace Psr\SimpleCache;
 
 /**
- * PHP 5 compatibility for \Throwable interface.
- */
-if (PHP_VERSION_ID < 70000 && !class_exists('\Throwable')) {
-    class_alias('Psr\SimpleCache\ThrowablePhp5', '\Throwable');
-}
-
-/**
  * Interface used for all types of exceptions thrown by the implementing library.
  */
 interface CacheException extends \Throwable
