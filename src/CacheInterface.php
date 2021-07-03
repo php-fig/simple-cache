da1657b1ebeb78c1d1504dd02ae30d4ba48d812d
Navarr Barnier <me@navarr.me>
2021-07-03 09:49:55 -0400

    Expand parameter types for PHP 8

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -15,7 +15,7 @@ interface CacheInterface
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *   MUST be thrown if the $key string is not a legal value.
      */
-    public function get(string $key, $default = null);
+    public function get(string $key, mixed $default = null);
 
     /**
      * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
@@ -31,7 +31,7 @@ interface CacheInterface
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *   MUST be thrown if the $key string is not a legal value.
      */
-    public function set(string $key, $value, $ttl = null);
+    public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null);
 
     /**
      * Delete an item from the cache by its unique key.
@@ -64,7 +64,7 @@ interface CacheInterface
      *   MUST be thrown if $keys is neither an array nor a Traversable,
      *   or if any of the $keys are not a legal value.
      */
-    public function getMultiple(iterable $keys, $default = null);
+    public function getMultiple(iterable $keys, mixed $default = null);
 
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
@@ -80,7 +80,7 @@ interface CacheInterface
      *   MUST be thrown if $values is neither an array nor a Traversable,
      *   or if any of the $values are not a legal value.
      */
-    public function setMultiple(iterable $values, $ttl = null);
+    public function setMultiple(iterable $values, null|int|\DateInterval $ttl = null);
 
     /**
      * Deletes multiple cache items in a single operation.
