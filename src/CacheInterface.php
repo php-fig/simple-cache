47af10ad50dd19692558a720e27a68953a81a66e
Navarr Barnier <me@navarr.me>
2021-07-14 12:27:49 -0400

    Update iterable doc types to identify values and in some cases keys

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -55,10 +55,10 @@ interface CacheInterface
     /**
      * Obtains multiple cache items by their unique keys.
      *
-     * @param iterable $keys    A list of keys that can be obtained in a single operation.
-     * @param mixed    $default Default value to return for keys that do not exist.
+     * @param iterable<string> $keys    A list of keys that can be obtained in a single operation.
+     * @param mixed            $default Default value to return for keys that do not exist.
      *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     * @return iterable<string, mixed> A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *   MUST be thrown if $keys is neither an array nor a Traversable,
@@ -85,7 +85,7 @@ interface CacheInterface
     /**
      * Deletes multiple cache items in a single operation.
      *
-     * @param iterable $keys A list of string-based keys to be deleted.
+     * @param iterable<string> $keys A list of string-based keys to be deleted.
      *
      * @return bool True if the items were successfully removed. False if there was an error.
      *
